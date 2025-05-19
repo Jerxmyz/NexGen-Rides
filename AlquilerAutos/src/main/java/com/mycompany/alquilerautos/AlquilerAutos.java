c1887acfd99feb08582d2a7809c4aa4b008aec1e
Jerxmyz <jjzambrano15@espe.edu.ec>
2025-05-19 14:59:01 -0500

    NexGenRides

---
+++ AlquilerAutos/src/main/java/com/mycompany/alquilerautos/AlquilerAutos.java
@@ -0,0 +1,10 @@
+package com.mycompany.alquilerautos;
+
+public class AlquilerAutos {
+
+    public static void main(String[] args) {
+        Cliente cliente = new Cliente("", 0, "", "");
+        cliente.pedirDatos();
+        cliente.mostrarDatos();
+    }
+}
